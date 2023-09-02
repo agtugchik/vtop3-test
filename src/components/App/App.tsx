b2f57817e1677d988a564272a2b98725c0c595a5
agtugchik <agtugchik13@gmail.com>
2023-09-02 22:42:14 +0300

    feat: add footer container

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,12 +2,14 @@ import Header from '../UI/Header/Header';
 import React from 'react';
 import cl from './App.module.scss';
 import Carousel from '../Carousel/Carousel';
+import Footer from '../UI/Footer/Footer';
 
 const App = () => {
   return (
     <div className={cl.app}>
       <Header />
       <Carousel />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/UI/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import cl from './Footer.module.scss';
+
+const Footer = () => {
+  return <footer className={cl.footer}></footer>;
+};
+
+export default Footer;
