74d122caf711bba8c4d80d019e3b7bb90d1ee90e
agtugchik <agtugchik13@gmail.com>
2023-09-02 22:51:07 +0300

    feat: change language switcher

--- src/components/UI/LanguageSwitch/LanguageSwitch.tsx
+++ src/components/UI/LanguageSwitch/LanguageSwitch.tsx
@@ -1,16 +1,15 @@
 import { Language } from '../../../constants';
 import React, { useState } from 'react';
 import ButtonType2 from '../ButtonType2/ButtonType2';
-import Link from '../Link/Link';
 import LanguageIcon from '../LanguageIcon/LanguageIcon';
-// import cl from './LanguageSwitch.module.scss';
+import cl from './LanguageSwitch.module.scss';
 
 const LanguageSwitch = () => {
   const [language, setLanguage] = useState<Language.EN | Language.RU>(Language.RU);
   return (
     <ButtonType2 type="headerButtonType2" state={[language, setLanguage]}>
       <LanguageIcon />
-      <Link title={language} />
+      <div className={cl.switcher}>{language}</div>
     </ButtonType2>
   );
 };
