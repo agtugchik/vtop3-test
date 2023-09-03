4c467c2feea9c4f09f1a93abf6b6efbf39528e91
agtugchik <agtugchik13@gmail.com>
2023-09-03 18:18:03 +0300

    feat: finish footer

--- src/components/UI/Footer/Footer.tsx
+++ src/components/UI/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import List from '../List/List';
+import SocialMedia from '../SocialMedia/SocialMedia';
 import cl from './Footer.module.scss';
 
 const Footer = () => {
@@ -9,6 +10,7 @@ const Footer = () => {
         className={cl.container}
         titles={['Terms & Conditions', 'Cookies', 'Contacts', 'Careers', 'Brand Guide']}
       />
+      <SocialMedia />
     </footer>
   );
 };
---
+++ src/components/UI/SocialMedia/SocialMedia.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import cl from './SocialMedia.module.scss';
+import Icons from '../Icons/Icons';
+
+const SocialMedia = () => {
+  return (
+    <div className={cl.socialMedia}>
+      <h3>Our social media:</h3>
+      <ul>
+        {Icons.map((Icon, i) => (
+          <li key={i}>
+            <a href="#">{Icon()}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SocialMedia;
