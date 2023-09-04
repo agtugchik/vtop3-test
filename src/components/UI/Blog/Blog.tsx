0af029e17ed96b2117b7058e8ccea937890c3f34
agtugchik <agtugchik13@gmail.com>
2023-09-04 18:36:07 +0300

    feat: improve blog logic

--- src/components/UI/Blog/Blog.tsx
+++ src/components/UI/Blog/Blog.tsx
@@ -3,6 +3,7 @@ import SimpleBar from 'simplebar-react';
 import cl from './Blog.module.scss';
 import 'simplebar-react/dist/simplebar.min.css';
 import BlogItem from '../BlogItem/BlogItem';
+import { blogItemDate, blogItemText } from '../../../constants';
 
 const Blog = () => {
   return (
@@ -10,9 +11,9 @@ const Blog = () => {
       <h2>BLOG</h2>
       <div className={cl.blog_area}>
         <SimpleBar style={{ maxHeight: 571 }}>
-          {[...Array(5)].fill(BlogItem).map((x, i) => (
-            <p key={i}>{x()}</p>
-          ))}
+          {[...Array(5)]
+            .fill(BlogItem)
+            .map((x) => (x as typeof BlogItem)({ title: blogItemText, date: blogItemDate }))}
         </SimpleBar>
       </div>
     </div>
--- src/components/UI/BlogItem/BlogItem.tsx
+++ src/components/UI/BlogItem/BlogItem.tsx
@@ -1,13 +1,17 @@
-import { blogItemDate, blogItemText } from '../../../constants';
-import React from 'react';
+import React, { FC } from 'react';
 import cl from './BlogItem.module.scss';
 
-const BlogItem = () => {
+interface IBlogItem {
+  title: string;
+  date: string;
+}
+
+const BlogItem: FC<IBlogItem> = ({ title, date }) => {
   return (
     <a href="">
       <div className={cl.blogItem}>
-        <h4>{blogItemText}</h4>
-        <p>{blogItemDate}</p>
+        <h4>{title}</h4>
+        <p>{date}</p>
       </div>
     </a>
   );
